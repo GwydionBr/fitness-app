89ef760297e0aaae2362619bb9ca108b3b1e54f8
GwydionBr <gwydie@googlemail.com>
2025-03-31 12:02:03 +0200

    enhance settings screen layout; add navigation links to account and workout tabs

--- app/(tabs)/(settings)/settings.tsx
+++ app/(tabs)/(settings)/settings.tsx
@@ -2,15 +2,31 @@ import { StyleSheet } from 'react-native'
 import React from 'react'
 import ThemedSafeAreaView from '@/components/ThemedSafeAreaView'
 import { ThemedText } from '@/components/ThemedText';
+import { Link } from 'expo-router';
 
 const settings = () => {
   return (
-    <ThemedSafeAreaView>
+    <ThemedSafeAreaView style={styles.container}>
       <ThemedText>settings</ThemedText>
+      <Link href="/(tabs)/(settings)/account" style={styles.link}>Go to account</Link>
+      <Link href="/(tabs)/(workout)/workout" style={styles.link}>Go to workout</Link>
     </ThemedSafeAreaView>
   );
 }
 
 export default settings
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  link: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: '#007AFF',
+    color: '#fff',
+    borderRadius: 5,
+  }
+})
